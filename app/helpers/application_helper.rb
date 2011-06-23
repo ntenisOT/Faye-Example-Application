1d84c69969a8c5d8e07d2b1f4ef86825922963f7
Georgios Larkou <ntenisOT@gmail.com>
2011-06-23 01:49:03 +0100

    is working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def broadcast(channel, &block)
     message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
-    uri = URI.parse("http://morning-flower-798.heroku.com/faye")
+    uri = URI.parse("http://young-light-581.heroku.com/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(function() {
-  var faye = new Faye.Client('http://morning-flower-798.heroku.com/faye');
+  var faye = new Faye.Client('http://young-light-581.heroku.com/faye');
   faye.subscribe("/messages/new", function(data) {
     eval(data);
   });
