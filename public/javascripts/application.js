a27e5e04f72bbbfadcb182942ca02254613032f6
Georgios Larkou <ntenisOT@gmail.com>
2011-06-23 03:07:17 +0100

    working finally

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(function() {
-  var faye = new Faye.Client('http://young-light-581.heroku.com/faye');
+  var faye = new Faye.Client('http://fayesedar.herokuapp.com/faye');
   faye.subscribe("/messages/new", function(data) {
     eval(data);
   });
