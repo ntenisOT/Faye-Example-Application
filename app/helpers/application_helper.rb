c354eccda82a08fd30d1ff3f5afcc1e5adb8b925
Georgios Larkou <ntenisOT@gmail.com>
2011-06-23 11:53:19 +0100

    server REDIS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def broadcast(channel, &block)
     message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
-    uri = URI.parse("http://fayeserverexample.herokuapp.com/faye")
+    uri = URI.parse("http://fayeredis.herokuapp.com/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(function() {
-  var faye = new Faye.Client('http://fayeserverexample.herokuapp.com/faye');
+  var faye = new Faye.Client('http://fayeredis.herokuapp.com/faye');
   faye.subscribe("/messages/new", function(data) {
     eval(data);
   });
